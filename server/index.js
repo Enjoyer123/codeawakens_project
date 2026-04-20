a7958f0408bbed7a0acdae2b85f07ab027b3d8e4
ART-Tippatai-Kongpayung <s6504062616071@email.kmutnb.ac.th>
2026-04-20 14:40:01 +0700

    fix json size largest request

--- server/index.js
+++ server/index.js
@@ -23,8 +23,8 @@ import { setupSwagger } from "./swagger.js";
 
 // ====== MIDDLEWARE ======
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 import path from "path";
 import { clerkMiddleware } from "@clerk/express";
 import morgan from "morgan";
@@ -48,8 +48,6 @@ app.use(
 
 // Morgan HTTP request logging for UAT evidence
 // app.use(morgan("combined"));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/api/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(clerkMiddleware());
 
